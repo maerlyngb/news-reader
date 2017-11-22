30622221532aa46ac936dec1e9a772c2ee532911
Maerlyn Broadbent <maerlyn.broadbent@gmail.com>
2017-11-22 15:56:14 +1100

    moved nav drawer logic into seperate class so it can be reused  in other activities

--- app/src/main/java/io/maerlyn/newsreader/MainActivity.java
+++ app/src/main/java/io/maerlyn/newsreader/MainActivity.java
@@ -1,48 +1,58 @@
 package io.maerlyn.newsreader;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
+
+    private NavDrawerHandler navDrawerHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.main_activity_title);
 
+        this.navDrawerHandler = new NavDrawerHandler(this);
+
+        SetupNavigationView(toolbar);
+    }
+
+    /**
+     * Setup the side navigation drawer
+     *
+     * @param toolbar to place the toggle button
+     */
+    private void SetupNavigationView(Toolbar toolbar) {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
+
+        // toggle button in action bar
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        // listen for nav items being clicked
         NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setNavigationItemSelectedListener(this.navDrawerHandler);
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
+    /**
+     * Triggered when creating the options menu in the toolbar
+     *
+     * @param menu to display in the toolbar
+     * @return boolean
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -50,6 +60,12 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    /**
+     * Triggered when one of the items in the options menu was tapped
+     *
+     * @param item that was tapped
+     * @return boolean
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -65,28 +81,16 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
+    /**
+     * Close the side nav pane when the back button is pressed.
+     * If the nav pane is already closed, perform the standard os action.
+     */
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+    public void onBackPressed() {
+        if (this.navDrawerHandler.isDrawerOpen()) {
+            this.navDrawerHandler.closeDrawer();
+        } else {
+            super.onBackPressed();
         }
-
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
